81d8a11e71ead1ba437fa667b21fdeb203570d13
Natnael <natnaelatnafu25@gmail.com>
2026-08-09 15:55:48 -0500

    part 5 References doen

--- part05-13_transpose_matrix/src/transpose_matrix.py
+++ part05-13_transpose_matrix/src/transpose_matrix.py
@@ -1,1 +1,20 @@
 # Write your solution here
+
+def transpose(matrix: list):
+
+    transposed = []
+
+    for c in range(len(matrix[0])):
+        new_row= []
+        for r in range(len(matrix)):
+            new_row.append(matrix[r][c])
+        transposed.append(new_row)
+
+    matrix[:] = transposed
+
+
+if __name__ == "__main__":
+    matrix = [[10, 100], [10, 100]]
+    transpose(matrix)
+    print("after")
+    print(matrix)
