73899ad66f2cf07b7ed695aa3b99ce9dc0b09520
Natnael <natnaelatnafu25@gmail.com>
2026-08-10 18:39:03 -0500

    Part 5 dictionary: times_ten and factorials functions with dictionary output

--- part05-14_times_ten/src/times_ten.py
+++ part05-14_times_ten/src/times_ten.py
@@ -1,1 +1,18 @@
 # Write your solution here
+
+def times_ten(start_index: int, end_index: int ):
+
+    dectionary = {}
+
+    count = end_index - start_index
+
+
+    for i in range(start_index, end_index +1 ):
+
+        dectionary[i] = i * 10
+
+    return dectionary
+
+
+if __name__ == "__main__":
+    print(times_ten(3, 6))
--- part05-15_factorials/src/factorials.py
+++ part05-15_factorials/src/factorials.py
@@ -1,1 +1,25 @@
 # Write your solution here
+
+def factorials(n: int):
+
+    factorial = 1
+
+
+    factorials = {}
+    
+
+    for i in range(factorial,n + 1):
+        factorial *= i
+        factorials[i] = factorial
+
+    return factorials
+
+        
+
+
+
+
+
+
+if __name__ == "__main__":
+    print(factorials(5))
