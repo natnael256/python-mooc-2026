92fbe819549785d30d5fc3d7b34e1107b6fc4b0d
Natnael <natnaelatnafu25@gmail.com>
2026-08-10 23:15:08 -0500

    histogram function to count character occurrences in a word

--- part05-16_histogram/src/histogram.py
+++ part05-16_histogram/src/histogram.py
@@ -1,1 +1,24 @@
 # Write your solution here
+
+def histogram(word):
+
+    count = {}
+
+
+    for i in word:
+        if i not in count:
+            count[i] = 1
+        else:
+            count[i] += 1
+
+    for j in count:
+
+        print (f"{j} {count[j] * '*'}")
+
+
+if __name__ == "__main__":
+
+    word = "statistically"
+
+    histogram(word)
+
