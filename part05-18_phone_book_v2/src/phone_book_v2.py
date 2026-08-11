e36b7f65ae8fec4842561c76efe8501dfbb28a8e
Natnael <natnaelatnafu25@gmail.com>
2026-08-11 13:47:39 -0500

    phone book functionality with search and add multple numbers/ name

--- part05-18_phone_book_v2/src/phone_book_v2.py
+++ part05-18_phone_book_v2/src/phone_book_v2.py
@@ -1,1 +1,35 @@
 # Write your solution here
+
+
+phone_book = {}
+
+
+
+while True:
+    command = input("command (1 search, 2 add, 3 quit): ")
+    if command == "1":
+        name = input("name: ")
+
+        if name in phone_book:
+            for number in phone_book[name]:
+                print(f"{number}")
+       
+        else:
+            print("no number")
+
+    elif command == "2":
+        name = input("name: ")
+        number = input("number: ")
+
+        if name in phone_book:
+            phone_book[name].append(number)
+            print("ok!")
+            # print(phone_book)
+        else: 
+            phone_book[name] = [number]
+            print("ok!")
+        
+    elif command == "3":
+        print("quitting...")
+        break
+# print(phone_book)
