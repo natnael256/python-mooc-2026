9f2de9cf7571eb8bfd1fd6b3f3ebe67e4ac1c4cb
Natnael <natnaelatnafu25@gmail.com>
2026-08-11 14:22:56 -0500

    invert function to swap dictionary keys and values

--- part05-19_invert_dictionary/src/invert_dictionary.py
+++ part05-19_invert_dictionary/src/invert_dictionary.py
@@ -1,1 +1,20 @@
 # Write your solution here
+
+
+def invert(dic: dict):
+
+    new_dic = {}
+    
+    for key in dic.keys():
+
+        j = dic.get(key)
+
+        new_dic[j] = key
+
+    dic.clear()
+    dic.update(new_dic)
+
+if __name__ == "__main__":
+    s = {1: "first", 2: "second", 3: "third", 4: "fourth"}
+    invert(s)
+    print(s)
