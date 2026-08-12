2c08190576d8177e5a4a4e8f823e1bf13cd24e1b
Natnael <natnaelatnafu25@gmail.com>
2026-08-12 00:11:13 -0500

    Part 5 dictionary done: add_movie and find_movies functions for movie database

--- part05-21_movie_database/src/movie_database.py
+++ part05-21_movie_database/src/movie_database.py
@@ -1,1 +1,31 @@
 # Write your solution here
+
+# Write your solution here
+
+
+def add_movie(database: list, name: str, director:str, year: int, runtime: int):
+    new_bd = {}
+
+    if len(new_bd) < 1:
+        new_bd["name"] = name
+        new_bd["director"] = director
+        new_bd["year"] = year
+        new_bd["runtime"] = runtime
+    else:
+        new_bd["name"].append(name)
+        new_bd["director"].append(director)
+        new_bd["year"].append(year)
+        new_bd["runtime"].append(runtime)
+
+    database.append(new_bd)
+
+
+
+
+
+if __name__ == "__main__":
+
+    database = []
+    add_movie(database, "Gone with the Python", "Victor Pything", 2017, 116)
+    add_movie(database, "Pythons on a Plane", "Renny Pytholin", 2001, 94)
+    print(database)
--- part05-22_find_movies/src/find_movies.py
+++ part05-22_find_movies/src/find_movies.py
@@ -1,1 +1,26 @@
 # Write your solution here
+
+def find_movies(database: list, search_term: str):
+
+    movies = []
+    for i in database:
+
+        if search_term.lower() in i["name"].lower():
+            movies.append(i)
+            
+    return movies
+            
+
+
+
+
+
+
+if __name__ == "__main__":
+
+    database = [{"name": "Gone with the Python", "director": "Victor Pything", "year": 2017, "runtime": 116},
+    {"name": "Pythons on a Plane", "director": "Renny Pytholin", "year": 2001, "runtime": 94},
+    {"name": "Dawn of the Dead Programmers", "director": "M. Night Python", "year": 2011, "runtime": 101}]
+
+    my_movies = find_movies(database, "python")
+    print(my_movies)
