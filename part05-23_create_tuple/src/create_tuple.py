8c3080597080b62fbc76ee4ca84fe897186edec9
Natnael <natnaelatnafu25@gmail.com>
2026-08-12 00:26:41 -0500

    Part 5 tuple: create_tuple function to return min, max, and sum of three integers

--- part05-23_create_tuple/src/create_tuple.py
+++ part05-23_create_tuple/src/create_tuple.py
@@ -1,1 +1,16 @@
 # Write your solution here
+
+
+
+def create_tuple(x: int, y: int, z: int):
+
+    num = [x, y, z]
+
+
+    tuple_num = (min(num), max(num), sum(num))
+
+    return tuple_num
+
+
+if __name__ == "__main__":
+    print(create_tuple(5, 3, -1))
